fcaefcaf44468c344c053b35fcb7a11a8ffd3716
Henrik Bulskov <bulskov@ruc.dk>
2020-11-19 12:02:26 +0100

    observable arrays

--- FrontendExample/wwwroot/js/viewModel.js
+++ FrontendExample/wwwroot/js/viewModel.js
@@ -1,5 +1,41 @@
-﻿define(['knockout'], function(ko) {
+﻿define(['knockout'], function (ko) {
+    //private part
+    let names = ko.observableArray(
+        [{ name: "Peter" }, { name: "Sue" }, { name: "Ellen" }]);
+    let firstName = ko.observable("Peter");
+    let lastName = ko.observable("Smith");
+    let fullName = ko.computed(function() {
+        return firstName() + " " + lastName();
+    });
+
+    let deleteName = function(data) {
+        names.remove(data);
+    }
+
+    let clickButton = function() {
+        names.push({ name: fullName() });
+        firstName("");
+        lastName("");
+
+    }
+
+
+    fetch("api/categories")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            names(data);
+        });
+
+
+    //public part
     return {
-        firstName: ko.observable("Peter")
+        firstName,
+        lastName,
+        fullName,
+        clickButton,
+        names,
+        deleteName
     };
 });
